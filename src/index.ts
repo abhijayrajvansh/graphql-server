654d6ee91a964941708d0b6d29b83a6df55f5b43
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-06-16 22:51:18 +0530

    todo: implement gql server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const PORT = process.env.PORT || 2711;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send({msg: "server is running"})
-})
+app.get("/", (req, res) => {
+  res.send({ msg: "server is running" });
+});
 
-app.listen(PORT, () => console.log(`server started on http://localhost:${PORT}`))
+app.get("/graphql", (req, res) => {
+  res.send("todo: implement graphql server");
+});
+
+app.listen(PORT, () =>
+  console.log(`server started on http://localhost:${PORT}`)
+);
