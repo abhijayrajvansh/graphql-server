d70609a867ded847913f277e19a392ce403d8fe2
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-06-19 05:15:56 +0530

    update: added graphql server

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,41 @@
 import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
 
-const app = express();
-const PORT = process.env.PORT || 2711;
+async function startServer() {
+  const PORT = process.env.PORT || 2711;
+  const app = express();
 
-app.use(express.json());
+  app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send({ msg: "server is running" });
-});
+  const gqlServer = new ApolloServer({
+    typeDefs:`
+      type Query {
+        hello: String
+        say(name: String): String
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => `hello from graphql!`,
+        say: (_, {name}: {name: String}) => `hello ${name}, how are you?` 
+      },
+    },
+  });
 
-app.get("/graphql", (req, res) => {
-  res.send("todo: implement graphql server");
-});
+  await gqlServer.start();
 
-app.listen(PORT, () =>
-  console.log(`server started on http://localhost:${PORT}`)
-);
+  app.get("/", (req, res) => {
+    res.json({ msg: "server is up and running" });
+  });
+
+  app.use("/graphql", expressMiddleware(gqlServer));
+
+  app.listen(PORT, () => {
+    console.log(
+      `> server started on PORT:${PORT}\n> Graphql-Studio is live at http://localhost:${PORT}/graphql.`
+    );
+  });
+}
+
+startServer();
